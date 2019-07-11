448d00c529dde0dcbda2f3c50e32b71f0dbb70bf
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-07-10 23:15:50 -0600

    processator contact

--- cli-magnament/src/org/hck/controllers/ProcessatorController.java
+++ cli-magnament/src/org/hck/controllers/ProcessatorController.java
@@ -4,13 +4,18 @@
  * and open the template in the editor.
  */
 package org.hck.controllers;
-
+import java.util.ArrayList;
+import org.hck.utils.ReadIO;
+import org.hck.beans.Contacts;
+import org.hck.beans.Category;
 /**
  *
  * @author Hackobo
  */
 public class ProcessatorController {
     private static ProcessatorController instance;
+    private String term = "";
+    
     
     public static ProcessatorController getInstance(){
         if( instance == null ){
@@ -26,6 +31,22 @@ public class ProcessatorController {
         System.out.println(" [ SHOW-CONTACT   ]  ");
         System.out.println(" [ UPDATE-CONTACT ] <IdContact> ");
         System.out.println(" [ DELETE-CONTACT ] <IdContact> ");
+        this.term = ReadIO.getInstance().IOData();
+        switch( this.term ){
+            case "ADD-CONTACT" :
+                System.out.println("holas add");
+            break;
+            case "SHOW-CONTACT":
+                System.out.println("holas shos");
+            break;
+            case "UPDATE-CONTACT":
+                System.out.println("holas updated");
+            break;
+            case "DELETE-CONTACT":
+            break;
+            default:
+                System.out.println(" Opcion invalida  ");
+        }
     }
     
 }
