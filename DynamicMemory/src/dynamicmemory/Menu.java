3555fcebec540289c3aa1b0d03c6d793cd560168
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-08-09 19:58:33 -0600

    set data in arrays

--- DynamicMemory/src/dynamicmemory/Menu.java
+++ DynamicMemory/src/dynamicmemory/Menu.java
@@ -15,8 +15,12 @@ import java.util.Scanner;
 public class Menu {
     private static Menu instance;
     private ArrayList<Empleado> emp = new ArrayList<Empleado>();
+    private ArrayList<Persona> per = new ArrayList<Persona>();
+    
     private Scanner sc = new Scanner(System.in);
     private  int optMenu = 0;
+    
+    private Persona persona = new Persona();
     
     public static Menu getInstance(){
         if( instance == null ){
@@ -40,7 +44,30 @@ public class Menu {
     }
     
     public void optsEmpleado(){
-        System.out.println("este es el  menu del empleado");
+        int op = 0;
+        System.out.println(" 1. INGRESAR ");
+        System.out.println(" 2. MOSTRAR  ");
+        op = Integer.parseInt( sc.nextLine());
+        if( op  == 1 ){
+            System.out.println("este es el  menu del empleado");
+            System.out.println("Nombre");
+            persona.setNombre( sc.nextLine() );
+            System.out.println("DPI");
+            persona.setDpi( sc.nextLine() );
+            System.out.println("Edad");
+            persona.setEdad( Integer.parseInt( sc.nextLine()));
+            per.add(persona);
+            this.optsGeneral();
+        }
+        
+        if( op == 2){
+            System.out.println("los valore son");
+            for( int i=0; i < per.size(); i ++ ){
+                System.out.println("Nombre : " + per.get(i).getNombre());
+                System.out.println("DPI" + per.get(i).getDpi());
+                System.out.println("Edad" + per.get(i).getEdad());
+            }
+        }    
     }
     
     public void optsPersona(){
