ffd0294375da07622ba275c0bd509125c975f156
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-10-16 22:53:54 -0600

    demo call-stack data-structures apply to JVM

---
+++ Call-Stack/src/org/hackobo/beans/Foo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hackobo.beans;
+
+/**
+ *
+ * @author Hackobo
+ */
+public class Foo {
+    
+}
