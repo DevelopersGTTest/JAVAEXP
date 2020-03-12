706e7dfaab08110888f5088d756a366bffb740e9
p-jacobo2012240 <pablojacobo1998@gmail.com>
2020-03-11 23:09:22 -0600

    last changes

--- simpleConnection/src/java/org/hackobo/utils/ConnectionController.java
+++ simpleConnection/src/java/org/hackobo/utils/ConnectionController.java
@@ -7,11 +7,17 @@ package org.hackobo.utils;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.hackobo.utils.Conexion;
 
 /**
  *
@@ -30,42 +36,26 @@ public class ConnectionController extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException, Exception {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            Conexion cnn = new Conexion();
+            cnn.open();
             
+            String sqlT = "INSERT INTO foo(idFoo, nameFoo) VALUES(3, 'JACOBO')";
+            boolean r = cnn.executeSql(sqlT);
+            
+            System.out.println(" valieee" + r );
+            
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+
 
     /**
      * Returns a short description of the servlet.
---
+++ simpleConnection/src/java/org/hackobo/utils/Foo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hackobo.utils;
+
+/**
+ *
+ * @author Hackobo
+ */
+public class Foo {
+    private int idFoo;
+    private String nameFoo;
+
+    public Foo(int idFoo, String nameFoo) {
+        this.idFoo = idFoo;
+        this.nameFoo = nameFoo;
+    }
+
+    public int getIdFoo() {
+        return idFoo;
+    }
+
+    public void setIdFoo(int idFoo) {
+        this.idFoo = idFoo;
+    }
+
+    public String getNameFoo() {
+        return nameFoo;
+    }
+
+    public void setNameFoo(String nameFoo) {
+        this.nameFoo = nameFoo;
+    }
+    
+    
+}
